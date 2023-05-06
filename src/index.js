e6728834135f2046ec2acafec1dc984b8752cd2f
Yakimenko Viktor <60235171+chell-iz-naroda@users.noreply.github.com>
2023-05-06 18:24:32 +0300

    update impot&include

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import './js/api-service';
 import './js/api-vars'
 import './js/team-modal';
 import './js/team';
+import './js/refs';
+import './js/render';
+import './js/searchMovie';
+import './js/trandings';
