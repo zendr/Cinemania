27a30260631ba393acd6a186f4b713951f86f54b
Yakimenko Viktor <60235171+chell-iz-naroda@users.noreply.github.com>
2023-05-07 23:40:53 +0300

    update pagination

--- src/js/trandings.js
+++ src/js/trandings.js
@@ -1,9 +1,13 @@
 import {getTrendData} from './api-service';
 import { renderMarkup } from './render';
-import { saveLs } from './storage';
+import { loadLs, saveLs, loadLs } from './storage';
 
-getTrendData(1).then((data) => {
-	renderMarkup(data),
-	//Сохранение результата запроса в  localStorage
+let searchPage = 1;
+
+
+getTrendData(searchPage).then((data) => {
+	renderMarkup(data);
+	saveLs('totalItems', data.total_results);
+	console.log(loadLs('totalItems'))
 	saveLs("moviesData", data.results);
 });
