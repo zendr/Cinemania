10c31813166d872221c66c9c19506dc0a419f338
TyomaShapoval <shapoval1044@gmail.com>
2023-05-07 13:48:48 +0300

    fixed classList interaction dom

--- src/js/header.js
+++ src/js/header.js
@@ -7,37 +7,51 @@ const mobMenu = document.getElementById("mobile");
 const blind = document.getElementById("blind")
 const body = document.querySelector("body")
 const backImg = document.getElementById("hero_box1")
-const backColor = document.getElementById("hero_box2")
-console.log(backColor.attributes.style.nodeValue)
+const backColor = document.getElementById("hero_box2");
+const hero = document.querySelector(".hero");
+console.log(hero)
 
-themeBtn.addEventListener("click", () => {
-if (themeBtn.dataset.theme === "false") {
-
-    themeMoon.classList.toggle("theme-moon-light");
-    themeElipse.classList.toggle("theme-elipse-ligth");
-    themeSun.classList.toggle("theme-sun-light");
-
-    themeBtn.dataset.theme = "true"
-
+function themeKit () {
+    if (themeBtn.dataset.theme === "false") {
     
-body.classList.add("theme_sunshine");
-backImg.classList.add("hero_box1-light")
-backColor.attributes.style.nodeValue = "background-color: #F8F8F8"
+        themeMoon.classList.toggle("theme-moon-light");
+        themeElipse.classList.toggle("theme-elipse-ligth");
+        themeSun.classList.toggle("theme-sun-light");
+    
+        themeBtn.dataset.theme = "true"
+    
+        
+    body.classList.add("theme_sunshine");
+    if (hero !== null){
+        backImg.classList.add("hero_box1-light");
+        backColor.attributes.style.nodeValue = "background-color: #F8F8F8";
+    }
+    localStorage.setItem("ui-theme", "light");
+    
+    
+    }else {
+    
+        themeMoon.classList.toggle("theme-moon-light");
+        themeElipse.classList.toggle("theme-elipse-ligth");
+        themeSun.classList.toggle("theme-sun-light");
+    
+        themeBtn.dataset.theme = "false"
+    
+        body.classList.remove("theme_sunshine");
+        if (hero !== null) {
+            backImg.classList.remove("hero_box1-light");
+            backColor.attributes.style.nodeValue = "background-color: #0e0e0e"
+        }
+        localStorage.setItem("ui-theme", "dark");
+    }
+    }
 
-
-}else {
-
-    themeMoon.classList.toggle("theme-moon-light");
-    themeElipse.classList.toggle("theme-elipse-ligth");
-    themeSun.classList.toggle("theme-sun-light");
-
-    themeBtn.dataset.theme = "false"
-
-    body.classList.remove("theme_sunshine");
-    backImg.classList.remove("hero_box1-light");
-    backColor.attributes.style.nodeValue = "background-color: #0e0e0e"
+if(localStorage.getItem("ui-theme") === "light") {
+    themeKit();
 }
-});
+
+
+themeBtn.addEventListener("click", themeKit);
 
 menuBtn.addEventListener("click", () => {
 mobMenu.classList.remove("is-hidden");
